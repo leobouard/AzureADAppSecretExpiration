bb095f16f2d8ce4ea0515c128be677f0e2c62e7e
Léo Bouard <leo.bouard@hotmail.com>
2023-06-09 16:12:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+﻿# AzureADAppsSecretExpiration
+
+Friendly reminder through Teams MessageCard for secret expiration on your Azure AD *App Registrations*.
+
+![Result of the MessageCard](/capture.png)
+
+## Parameters
+
+There is two parameters available on the script:
+
+1. `-Webhooks` to provide the URI of the Teams incoming webhooks. Multiple URIs are accepted.
+2. `-Days` to filter secret that will expire in X days or less. By default, the value of this parameter is 30.
+
+## Message card
+
+Each section contains an expiring/expired secret, so you can have multiple section for one app (since an application can contains multiple secrets).
+
+## Useful resources
+
+- [MessageCard Playground V2](https://messagecardplayground.azurewebsites.net/) to see the result without having to create an incoming webhook
+- [Create an Incoming Webhook - Teams \| Microsoft Learn](https://learn.microsoft.com/en-us/microsoftteams/platform/webhooks-and-connectors/how-to/add-incoming-webhook)
---
+++ script.ps1
@@ -0,0 +1,102 @@
+﻿param(
+    [array]$Webhooks,
+    [int]$Days = 30
+)
+
+function Get-Expiration {
+    param([datetime]$EndDateTime)
+
+    $days = [int](New-TimeSpan -End $EndDateTime).TotalDays
+    if ($days -lt 0) { 
+        'that has **already expired**'
+    } else {
+        "that will expire in **$days days**"
+    }
+}
+
+Connect-MgGraph
+
+$apps = Get-MgApplication
+$date = (Get-Date).AddDays($Days)
+
+$apps = $apps | Where-Object {
+    ($_.PasswordCredentials -and $_.PasswordCredentials.EndDateTime -lt $date) -or
+    ($_.KeyCredentials -and $_.KeyCredentials.EndDateTime -lt $date)
+}
+
+[string]$body = @'
+{
+	"@type": "MessageCard",
+	"@context": "https://schema.org/extensions",
+	"summary": "Azure AD certificates & secret expiration",
+	"themeColor": "0078D7",
+	"title": "Azure AD certificates & secret expiration",
+	"sections": {{SECTIONS}}
+}
+'@
+
+[string]$sectionTemplate = @'
+{
+    "startGroup": true,
+    "activityTitle": "$appName",
+    "activitySubtitle": "$appId",
+    "text": "This Azure AD application uses a $type $expiration",
+    "facts": $facts,
+    "potentialAction": [
+        {
+            "@type": "OpenUri",
+            "name": "View in Azure",
+            "targets": [
+                {
+                    "os": "default",
+                    "uri": "$uri"
+                }
+            ]
+        }
+    ]
+}
+'@
+
+$sections = $apps | ForEach-Object {
+
+    $appName = $_.DisplayName
+    $appId   = $_.AppId
+    $uri     = "https://portal.azure.com/#view/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/~/Credentials/appId/$appId/isMSAApp~/false"
+    $owners  = Get-MgApplicationOwner -ApplicationId $_.Id | ForEach-Object { (Get-MgUser -UserId $_.Id).DisplayName }
+    $owners  = $owners -join ', '
+
+    $_.PasswordCredentials | Where-Object {$_.EndDateTime -lt $date} | ForEach-Object {
+        $type = 'client secret'
+        $expiration = Get-Expiration -EndDateTime $_.EndDateTime
+        $hint = if ($_.Hint) { $_.Hint + '******' } else { '' }
+        $facts = ([ordered]@{
+            'Secret hint' = $hint
+            Owners        = $owners -join ', '
+            Expiration    = Get-Date $_.EndDateTime -Format 'yyyy-MM-dd HH:mm:ss'
+        }).GetEnumerator() | ForEach-Object {[PSCustomObject]@{name=$_.Key;value=$_.Value}} | ConvertTo-Json
+        $ExecutionContext.InvokeCommand.ExpandString($sectionTemplate)
+        Clear-Variable type,hint,facts
+    }
+
+    $_.KeyCredentials | Where-Object {$_.EndDateTime -lt $date} | ForEach-Object {
+        $type = 'certificate'
+        $expiration = Get-Expiration -EndDateTime $_.EndDateTime
+        $facts = ([ordered]@{
+            'Certificate name' = $_.DisplayName
+            Owners        = $owners -join ', '
+            Expiration    = Get-Date $_.EndDateTime -Format 'yyyy-MM-dd HH:mm:ss'
+        }).GetEnumerator() | ForEach-Object {[PSCustomObject]@{name=$_.Key;value=$_.Value}} | ConvertTo-Json
+        $ExecutionContext.InvokeCommand.ExpandString($sectionTemplate)
+        Clear-Variable type,facts
+    }
+
+    Clear-Variable appName,appId,uri,owners
+}
+
+$sections = "[`n" + ($sections -join ",`n") + "`n]"
+$body = $body -replace '{{SECTIONS}}',$sections
+$bodyAsBytes = [System.Text.Encoding]::UTF8.GetBytes($body)
+
+$Webhooks | ForEach-Object {
+    Invoke-RestMethod -Method POST -Body $bodyAsBytes -Uri $_ -ContentType 'Application/Json'
+}
